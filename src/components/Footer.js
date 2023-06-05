ddeab17bbab589b1726201ce298dfab0a9df9b20
Milywayz <jtjenks@gmail.com>
2023-06-05 13:30:23 -0500

    finished footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,27 +1,31 @@
 import React from "react";
 import { DiGithubBadge } from "react-icons/di";
+import { TbBrandLinkedin } from "react-icons/tb";
+import { TbBrandTwitter } from "react-icons/tb";
 
 function Footer() {
   return (
-    <footer className="page-footer">
-      <div className="container">
-        <div className="row">
-          <div className="col l6 s12">
-            <i className="Icons">
-              <DiGithubBadge />
-            </i>
-          </div>
-          <div className="col l6 s12">
-            <i className="Icons">
-              <DiGithubBadge />
-            </i>
-            <div className="col l6 s12">
-              <i className="Icons">
-                <DiGithubBadge />
-              </i>
-            </div>
-          </div>
-        </div>
+    <footer className="page-footer icons cyan lighten-3">
+      <div className="col l6 s12">
+        <a href="https://github.com/Milywayz">
+          <i className="icons">
+            <DiGithubBadge />
+          </i>
+        </a>
+      </div>
+      <div className="col l6 s12">
+        <a href="https://www.linkedin.com/in/jeremy-jenkins-360a2b172/">
+          <i className="icons">
+            <TbBrandLinkedin />
+          </i>
+        </a>
+      </div>
+      <div className="col l6 s12">
+        <a href="https://twitter.com/Mily_Ways">
+          <i className="icons">
+            <TbBrandTwitter />
+          </i>
+        </a>
       </div>
     </footer>
   );

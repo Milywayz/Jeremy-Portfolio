4955ec96c9b58177f2d23e3af081d200952b8421
Milywayz <jtjenks@gmail.com>
2023-06-04 21:12:23 -0500

    finished navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,19 +3,36 @@ import React from "react";
 function Nav() {
   return (
     <nav>
-      <div class="nav-wrapper">
-        <a href="#" class="brand-logo right">
-          Logo
+      <div class="nav-wrapper cyan lighten-4">
+        <a
+          href="#!"
+          class="navHeader brand-logo center blue-text text-blue darken-2"
+        >
+          Jeremy's Portfolio
         </a>
-        <ul id="nav-mobile" class="left hide-on-med-and-down">
-          <li>
-            <a href="sass.html">Sass</a>
+        <ul class="left hide-on-med-and-down">
+          <li class="active">
+            <a href="sass.html" class="blue-text text--blue darken-2 bold">
+              About Me
+            </a>
           </li>
           <li>
-            <a href="badges.html">Components</a>
+            <a href="sass.html" class="blue-text text--blue darken-2 bold">
+              Portfolio
+            </a>
           </li>
           <li>
-            <a href="collapsible.html">JavaScript</a>
+            <a href="badges.html" class="blue-text text--blue darken-2 bold">
+              Contact
+            </a>
+          </li>
+          <li>
+            <a
+              href="collapsible.html"
+              class="blue-text text--blue darken-2 bold"
+            >
+              Resume
+            </a>
           </li>
         </ul>
       </div>
